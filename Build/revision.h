e6a868e2aa7cb34ee2d2eb5661f3159ce63a5b0c
mferrill <matthew_ferrill@yahoo.com>
2012-08-11 14:32:14 -0400

    Initial Commit

---
+++ Build/revision.h
@@ -0,0 +1,11 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _HASH                      "63d5a90394f5+"
+ #define _DATE                      "2012-07-23 01:19:16 -0700"
+ #define VER_COMPANYNAME_STR        "TrinityCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2012 TrinityCore"
+ #define VER_FILEVERSION            0,0,0
+ #define VER_FILEVERSION_STR        "2012-07-23 01:19:16 -0700 (63d5a90394f5+)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_H__
